4b3bc5f840557425bdd4e9b812893f5e3348393b
tranduc2204 <100020489+tranduc2204@users.noreply.github.com>
2026-07-28 02:10:29 +0000

    solve: 2 problems (2026-07-28)
    - py-rectangle-area
    - py-reverse-code

---
+++ practice/python/easy/py-rectangle-area/solution.py
@@ -0,0 +1,8 @@
+# Xom Data · Billboard area
+# Problem: https://xomdata.com/practice/py-rectangle-area
+# Solved: 2026-07-28
+
+def rectangle_area(width, height):
+    return width * height
+if __name__ == "_main_":
+    rectangle_area (5,3)
---
+++ practice/python/easy/py-reverse-code/solution.py
@@ -0,0 +1,6 @@
+# Xom Data · Reverse the order code
+# Problem: https://xomdata.com/practice/py-reverse-code
+# Solved: 2026-07-28
+
+def reverse_code(code):
+    return "".join(reversed(code))
