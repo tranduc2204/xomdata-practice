db77d1738d5c45c8b3739a013177bed9881f89f4
tranduc2204 <100020489+tranduc2204@users.noreply.github.com>
2026-08-31 04:26:49 +0000

    solve(python/easy): py-in-range

---
+++ practice/python/easy/py-in-range/solution.py
@@ -0,0 +1,9 @@
+# Xom Data · Check safe temperature
+# Problem: https://xomdata.com/practice/py-in-range
+# Solved: 2026-08-31
+
+def in_range(value, low, high):
+    if value >= low and value <= high:
+        return True
+    else: 
+        return False
